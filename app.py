ec3d1862d1880aff9c95c7e3a65496a321e32862
Zachary <tsai880806@gmail.com>
2024-08-18 00:44:48 +0800

    update docker-compose.yml

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from adapter.database import init_db
 from adapter.controller.product.product_controller import product_blueprint
 
 app = Flask(__name__)
-# For run on loaclhost
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://GainMiles:GainMiles@localhost:5433/gain_miles'
+# * For run on loaclhost 
+# * {$DataBaseName}://{$UserName}:{$Password}@localhost:5433/gain_miles
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://GainMiles:GainMiles@localhost:5433/gain_miles'
 
-# For docker compose using
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://GainMiles:GainMiles@db:5432/gain_miles'
+# * For docker compose using
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://GainMiles:GainMiles@db:5432/compose_pg_for_ci_cd_practice'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 init_db(app)
